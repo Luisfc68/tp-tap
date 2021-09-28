8e8ee435341da03d9823c73db2bb6bffe95c01fb
Luisfc68 <Luisfc68@gmail.com>
2021-09-28 20:44:31 -0300

    Mueve entidades a carpeta entity

---
+++ src/business-logic/entity/MidPlan.ts
@@ -0,0 +1,18 @@
+import { SubscriptionPlan } from "../bl.interfaces";
+
+export default class MidPlan implements SubscriptionPlan{
+    
+    static readonly MAX_CHATS_MIDPLAN:number = 5;
+
+    constructor(
+        private _createdChats:number
+    ) {}
+
+    registerChatCreation(): void {
+        this._createdChats++;
+    }
+
+    canCreateChat(): boolean {
+        return this._createdChats<MidPlan.MAX_CHATS_MIDPLAN;
+    }
+}

4e698633bdb2e6a501dcabb92748daa9b11117fc
Luisfc68 <Luisfc68@gmail.com>
2021-10-04 23:45:50 -0300

    Convierte a favChats en array en factory

--- src/business-logic/factory/UserFactory.ts
+++ src/business-logic/factory/UserFactory.ts
@@ -8,25 +8,25 @@ import User from "../entity/User";
 export default class UserFactory{
 
     private createUser(imgUrl:string, username:string, password:string,email: string,
-        plan:SubscriptionPlan,favChats?:Set<Chat>,id?:string):User{
+        plan:SubscriptionPlan,favChats?:Chat[],id?:string):User{
 
             return new User(imgUrl,username,password,email,plan,favChats,id);
     }
 
     createRegularUser(imgUrl:string, username:string, password:string,
-        email: string,favChats?:Set<Chat>,id?:string):User{
+        email: string,favChats?:Chat[],id?:string):User{
 
             return this.createUser(imgUrl,username,password,email,new RegularPlan(false),favChats,id);
     }
     
     createMidUser(imgUrl:string, username:string, password:string,
-        email: string,favChats?:Set<Chat>,id?:string):User{
+        email: string,favChats?:Chat[],id?:string):User{
 
             return this.createUser(imgUrl,username,password,email,new MidPlan(0),favChats,id);
     }
 
     createPremiumUser(imgUrl:string, username:string, password:string,
-        email: string,favChats?:Set<Chat>,id?:string):User{
+        email: string,favChats?:Chat[],id?:string):User{
 
             let endDate = new Date();
             endDate.setMonth(endDate.getMonth()+1); //Un mes en el futuro

cd0655d202cfe32e8f28922ff04b2fdcc4e14335
Luisfc68 <Luisfc68@gmail.com>
2021-10-11 17:52:11 -0300

    Mapea subscription plans con la base

--- src/business-logic/entity/MidPlan.ts
+++ src/business-logic/entity/MidPlan.ts
@@ -1,12 +1,22 @@
 import { SubscriptionPlan } from "../bl.interfaces";
+import { Model, MongoosePlugin } from "@tsed/mongoose";
+import { Name, Property } from "@tsed/schema";
 
+@Model()
+@MongoosePlugin(MidPlan.resolve)
 export default class MidPlan implements SubscriptionPlan{
     
+    
+    @Property()
+    @Name("createdChats")
+    private _createdChats:number;
+    
     static readonly MAX_CHATS_MIDPLAN:number = 5;
+    static resolve = () => MidPlan;
 
-    constructor(
-        private _createdChats:number
-    ) {}
+    constructor(_createdChats:number) {
+        this._createdChats = _createdChats;
+    }
 
     registerChatCreation(): void {
         this._createdChats++;
--- src/business-logic/entity/RegularPlan.ts
+++ src/business-logic/entity/RegularPlan.ts
@@ -1,10 +1,20 @@
 import { SubscriptionPlan } from "../bl.interfaces";
+import { Name, Property } from "@tsed/schema"
+import { Model, MongoosePlugin } from "@tsed/mongoose";
 
+@Model()
+@MongoosePlugin(RegularPlan.resolve)
 export default class RegularPlan implements SubscriptionPlan{
 
-    constructor(
-        private _haveCreatedChat:boolean
-    ){}
+    @Property()
+    @Name("haveCreatedChat")
+    private _haveCreatedChat:boolean;
+
+    static resolve = () => RegularPlan;
+
+    constructor(_haveCreatedChat:boolean){
+        this._haveCreatedChat = _haveCreatedChat;
+    }
 
     registerChatCreation(): void {
         this._haveCreatedChat = true;  

cedabe7fe3dfd61581195685e555d7379c5b7c02
Luisfc68 <Luisfc68@gmail.com>
2021-10-11 18:26:18 -0300

    Completa MongoUserDao

--- src/controllers/PruebasController.ts
+++ src/controllers/PruebasController.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Inject } from "@tsed/common";
-import { deserialize } from "@tsed/json-mapper";
-import {  Post, Returns } from "../../node_modules/@tsed/schema/lib";
+import { Controller, Get, Inject, QueryParams } from "@tsed/common";
+import { Returns } from "../../node_modules/@tsed/schema/lib";
+import PremiumPlan from "../business-logic/entity/PremiumPlan";
 import User from "../business-logic/entity/User";
-import ChatFactory from "../business-logic/factory/ChatFactory";
 import UserFactory from "../business-logic/factory/UserFactory";
 import { ChatDao, UserDao } from "../data-access/da.interfaces";
 import MongoChatDao from "../data-access/mongo/MongoChatDao";
@@ -16,11 +15,11 @@ import MongoUserDao from "../data-access/mongo/MongoUserDao";
 export class PruebasController {
   
   private userDao:UserDao;
-  private chatDao:ChatDao
+  //private chatDao:ChatDao
   
-  constructor(@Inject(MongoUserDao) userDao:UserDao, @Inject(MongoChatDao) chatDao:ChatDao){
+  constructor(@Inject(MongoUserDao) userDao:UserDao, @Inject(MongoChatDao) _chatDao:ChatDao){
     this.userDao = userDao;
-    this.chatDao = chatDao;
+    //this.chatDao = chatDao;
   }
   
   
@@ -34,51 +33,126 @@ export class PruebasController {
   async db(): Promise<User|null> {
 
     let fu: UserFactory = new UserFactory();
-    let fc: ChatFactory = new ChatFactory();
-
+    //let fc: ChatFactory = new ChatFactory();
     
-    let u1: User|null = fu.createRegularUser("https://i.imgur.com/zPFuLVO.jpeg","luisfc68","123","prueba@correo");
-    
-    u1 = await this.userDao.insert(u1);
+    let u1: User = fu.createRegularUser("https://i.imgur.com/zPFuLVO.jpeg","luisfc68","123","prueba@correo");
+    return this.userDao.insert(u1).then(u => {
 
-    let c = fc.createChat("mi chat","none","algo",u1,["1","a","<"]);
+      console.log(u.favChats)
 
-    c = await this.chatDao.insert(c);
-
-    u1.favChats.push(c);
-
-    u1 = await this.userDao.update(u1);
-
-
-    return u1;
+      return u;
+    });
   }
 
-  /*@Get("/mongoose/leer")
-  async leer(){
-    return this.servicio.leer("luisfc68");
-  }*/
-
-  @Post("/test")
-  @Returns(200,User).Groups("userRepresentation")
-  getSimpleRepresentation() {
-
-    let uf: UserFactory = new UserFactory();
-    let cf: ChatFactory = new ChatFactory();
+  @Get("/plans")
+  async planes():Promise<User[]>{
     
-    let user:User = uf.createRegularUser("imageUrl","example","123","example@");
-    let chat = cf.createChat("title","chatImg","some description",user,["tag1","tag2"]);
-    user.favChats.push(chat)
+    let fu: UserFactory = new UserFactory();
+    let premium = fu.createPremiumUser("https://i.imgur.com/zPFuLVO.jpeg","premium","123","correo1");
+    let mid = fu.createMidUser("https://i.imgur.com/zPFuLVO.jpeg","mid","456","correo2");
+    let normal = fu.createRegularUser("https://i.imgur.com/zPFuLVO.jpeg","normal","789","correo3");
 
-    console.log(deserialize(user,{groups: ['userRepresentation']}))
-    console.log(deserialize(chat,{groups: ['chatRepresentation']}))
+    let res:User[] = [];
 
-    return user;
-    
+    return this.userDao.insert(premium)
+      .then((p) => {
+        res.push(p);
+        return this.userDao.insert(mid);
+      })
+      .then((m) => {
+        res.push(m);
+        return this.userDao.insert(normal);
+      })
+      .then((n) => {
+        res.push(n);
+        res.forEach(u => {
+          console.log(u.plan)
+          console.log(u.plan.canCreateChat())
+        })
+        return res;
+      });
+
   }
 
-  @Get("/serv")
-  prueba(){
-    this.chatDao.insert(null as any);
+  @Get("/toPremium")
+  async update(@QueryParams("id") id: string):Promise<User|null>{
+    
+    let u:User|null = await this.userDao.get(id);
+    
+    if(!u)
+      return null;
+
+    u.plan = new PremiumPlan(new Date());
+
+    u = await this.userDao.update(u);
+
+    console.log("IMP => "+(<any>u?.plan).implementation);
+    console.log("PLAN => "+u?.plan.canCreateChat());
+
+    return u;
+
+  }
+
+  @Get("/read")
+  async read(@QueryParams("id") id: string):Promise<User|null>{
+    
+    let u:User|null = await this.userDao.get(id);
+
+    if(!u)
+      return null;
+
+    console.log("IMP => "+(<any>u.plan).implementation);
+    console.log("PLAN => "+u.plan.canCreateChat());
+
+    return u;
+
+  }
+
+  @Get("/all")
+  async all():Promise<User[]>{
+    
+    let u:User[] = await this.userDao.getAll(1);
+
+    u.forEach(r => {
+      console.log("IMP => "+(<any>r.plan).implementation);
+      console.log("PLAN => "+r.plan.canCreateChat());
+    });
+
+    return u;
+
+  }
+
+  @Get("/delete")
+  async delete(@QueryParams("id") id: string):Promise<User|null>{
+    
+    let u:User|null = await this.userDao.delete(id);
+
+    if(!u)
+      return null;
+
+    console.log("IMP => "+(<any>u.plan).implementation);
+    console.log("PLAN => "+u.plan.canCreateChat());
+
+    return u;
+
+  }
+
+  @Get("/nullpremium")
+  async nullpremium():Promise<User|null>{
+    
+    let fu: UserFactory = new UserFactory();
+
+    let u:User|null = fu.createRegularUser("a","a","a","a",[],"6164a9b084f25f011712bdea");
+    u = await this.userDao.update(u);
+
+    if(!u)
+      return null;
+
+    console.log("IMP => "+(<any>u.plan).implementation);
+    console.log("PLAN => "+u.plan.canCreateChat());
+
+    return u;
+
   }
 
 }
--- src/data-access/mongo/MongoUserDao.ts
+++ src/data-access/mongo/MongoUserDao.ts
@@ -3,25 +3,28 @@ import { MongooseModel } from "@tsed/mongoose";
 import User from "../../business-logic/entity/User";
 import { UserDao } from "../da.interfaces";
 import MongoEntityDao from "./MongoEntityDao";
+import * as Mongoose from "mongoose";
 
 export default class MongoUserDao extends MongoEntityDao<User> implements UserDao{
 
     constructor(
         @Inject(User) model:MongooseModel<User>
     ){
-        super(model,{
+        super(model,
+        {
             useFindAndModify: false
-        });
+        },["_favChats"]);
     }
 
-    update(obj: User): Promise<User|null> {
-        // TODO agregar para que actualice plan
+    update(param: User): Promise<User|null> {
+
         const update = {
-            _imgUrl: obj.imgUrl,
-            _username: obj.username,
-            _password: obj.password,
-            _email: obj.email,
-            _favChats: obj.favChats
+            _imgUrl: param.imgUrl,
+            _username: param.username,
+            _password: param.password,
+            _email: param.email,
+            _favChats: param.favChats,
+            _plan: param.plan
         };
 
         const ops = {
@@ -29,16 +32,89 @@ export default class MongoUserDao extends MongoEntityDao<User> implements UserDa
             ...super.generalOps
         };
        
-        return  super.model.findByIdAndUpdate(obj.id,[{ $set: update }],ops)
-                .then(obj => {
-                    if(obj)
-                        super.populate(obj);
-                    return obj?.toClass() || null;
+        return  super.model.findByIdAndUpdate(param.id,update,ops)
+                .then((obj) => {
+                    if(!obj)
+                        return null;
+                    return this.savePlanImplementation(param);
                 })
                 .catch(err => {
                     console.error(err);
                     throw new Error("Error updating document");
                 });
+    }
+
+    
+    insert(obj:User):Promise<User>{
+        return super.insert(obj).then(u => {
+            return this.savePlanImplementation(u);
+        })
+        .catch(err => {
+            console.log(err);
+            throw new Error("Error inserting document");
+        });
+    }
+
+    get(id:string):Promise<User|null>{
+        return super.get(id).then(u => {
+            if(!u)
+                return null;
+            this.loadPlanImplementation(u);
+            return u;
+        })
+        .catch( err => {
+            console.log(err);
+            throw new Error("Error determinating susbcription plan");
+        });
+    }
+
+    getAll(offset:number = 0):Promise<User[]>{
+        return super.getAll(offset)
+               .then(res => {
+                   res.forEach(u => this.loadPlanImplementation(u));
+                   return res;
+               })
+               .catch(err => {
+                    console.log(err);
+                    throw new Error("Error determinating susbcription plan");
+                });
+    }
+
+    delete(id:string):Promise<User|null>{
+        return super.delete(id).then(u => {
+            if(!u)
+                return null;
+            this.loadPlanImplementation(u);
+            return u;
+        })
+        .catch( err => {
+            console.log(err);
+            throw new Error("Error determinating susbcription plan");
+        });
+    }
+
+    //Guarda la implementacion de SubscriptionPlan para parsear al extraer de la base
+    private savePlanImplementation(u :User):Promise<User>{
+        return super.model.findByIdAndUpdate(u.id,
+            { 
+                $set: { "_plan.implementation" : u.plan.constructor.name} 
+            },
+            super.generalOps)
+        .then(doc => {
+            if(!doc)
+                throw new Error("Error saving subscription plan");
+            return u;
+        });
+    }
+
+    //Parsea la implementacion guardada y borra la propiedad en la que se guardo
+    private loadPlanImplementation(u :User){
+
+        const implementation:string = (<any>u.plan).implementation;
+        const model:any = Mongoose.model(implementation);
+
+        if(delete (<any>u.plan).implementation && model)
+            Object.setPrototypeOf(u.plan,model.resolve().prototype);
     }
 
 }

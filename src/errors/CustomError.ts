0d783a1a7b53f8feb4054a7355f118504848cd6d
luisfc68 <luis.chavez@xcaleconsulting.com>
2021-10-14 13:06:23 -0300

    Corrige errores personalizados

--- src/errors/CustomError.ts
+++ src/errors/CustomError.ts
@@ -1,11 +1,20 @@
 export default abstract class CustomError extends Error{
 
-    readonly isCustomError:boolean;
-    readonly modifier?:string;
+    private _isCustomError:boolean;
+    private _modifier?:string;
 
     constructor(msg:string,_modifier?:string,_isCustomError:boolean = true){
         super(msg);
-        this.modifier = _modifier;
-        this.isCustomError = _isCustomError;
+        this._modifier = _modifier;
+        this._isCustomError = _isCustomError;
     }
+
+    get isCustomError(){
+        return this._isCustomError;
+    }
+
+    get modifier(){
+        return this._modifier;
+    }
+
 }
--- src/errors/DaoError.ts
+++ src/errors/DaoError.ts
@@ -4,7 +4,6 @@ export default class DaoError extends CustomError{
 
     constructor(msg:string,modifier?:string){
         super(msg,modifier);
-        //Object.setPrototypeOf(this,CustomError.prototype);
     }
 
 }

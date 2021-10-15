a0f901f52f2603097861a40180758c7b8880b945
luisfc68 <luis.chavez@xcaleconsulting.com>
2021-10-15 19:32:27 -0300

    Completa endpoints basicos

--- src/controllers/ChatController.ts
+++ src/controllers/ChatController.ts
@@ -1,8 +1,8 @@
-import { $log, BodyParams, Req } from "@tsed/common";
+import { $log, BodyParams, PathParams, Req } from "@tsed/common";
 import { Controller, Inject } from "@tsed/di";
 import { BadRequest, Forbidden, NotFound } from "@tsed/exceptions";
 import { Authorize } from "@tsed/passport";
-import { Post, Returns } from "@tsed/schema";
+import { Delete, Post, Put, Returns } from "@tsed/schema";
 import Chat from "../business-logic/entity/Chat";
 import User from "../business-logic/entity/User";
 import ChatFactory from "../business-logic/factory/ChatFactory";
@@ -26,6 +26,18 @@ export default class ChatController{
         if(!id)
             throw new BadRequest("Invalid user ID");
         return id;
+    }
+
+    private isOwner(userId:string, chat:Chat):boolean{
+        const ownerId = (<User>chat.owner)._id?.toString();
+        return userId === ownerId;
+    }
+
+    private checkOperationConditions(userId:string, chat:Chat|null):void{
+        if(!chat)
+            throw new NotFound("Chat not found");
+        else if(!this.isOwner(userId,chat))
+            throw new Forbidden("You are not the owner of the chat");
     }
 
     @Post()
@@ -62,4 +74,46 @@ export default class ChatController{
 
     }
 
+    @Delete("/:chatId")
+    @Authorize("jwt")
+    @Returns(200,Chat).Groups(AppGroups.CHAT)
+    deleteChat(@Req() req:Req, @PathParams("chatId") chatId:string):Promise<Chat>{
+        const userId = this.verifyId(req);
+        return  this.chatDao.get(chatId)
+                .then(chat => {
+                    this.checkOperationConditions(userId,chat);
+                    return this.chatDao.delete(chatId);
+                })
+                .then(chat => chat!)
+                .catch(err => {
+                    $log.error("CATCHED CHATDAO EXCEPTION ON DELETECHAT ENDPOINT");
+                    $log.error(err);
+
+                    throw new BadRequest(err.message);
+                });
+    }
+    
+    @Put("/:chatId")
+    @Authorize("jwt")
+    @Returns(200,Chat).Groups(AppGroups.CHAT)
+    updateChat(@Req() req:Req, @PathParams("chatId") chatId:string, @BodyParams(Chat) reqChat:Chat):Promise<Chat>{
+        const userId = this.verifyId(req);
+        return  this.chatDao.get(chatId)
+                .then(chat => {
+
+                    this.checkOperationConditions(userId,chat);
+                    chat!.title = reqChat.title;
+                    chat!.description = reqChat.description;
+                    chat!.tags = reqChat.tags;
+
+                    return this.chatDao.update(chat!);
+                })
+                .then(chat => chat!)
+                .catch(err => {
+                    $log.error("CATCHED CHATDAO EXCEPTION ON UPDATECHAT ENDPOINT");
+                    $log.error(err);
+
+                    throw new BadRequest(err.message);
+                });
+    }
 }

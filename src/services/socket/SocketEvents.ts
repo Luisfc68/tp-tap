3e0bb365034cf109a27f485d22fc40f2bd882bf1
Luisfc68 <Luisfc68@gmail.com>
2021-10-20 00:49:21 -0300

    Registra eventos hasta este punto

--- src/services/socket/SocketEvents.ts
+++ src/services/socket/SocketEvents.ts
@@ -1,4 +1,11 @@
 export enum SocketEvents{
     ERROR="error",
-    DISCONECT="disconnect"
+    DISCONECT="disconnect",
+    JOIN_ROOM="join-room",
+    CHAT_REQUEST="chat-request",
+    MSG_REQUEST="msg-request",
+    LEAVE_ROOM="leave-room",
+    MSG_SENT="msg-sent"
 }
+
+// AGREGAR FAV CHAT Y QUITAR FAV CHAT
--- src/services/socket/events/sets/devSet.ts
+++ src/services/socket/events/sets/devSet.ts
@@ -1,8 +1,16 @@
 import { EventConstructor } from "../../socket.interfaces";
+import JoinRoom from "../implementations/JoinRoom";
+import LeaveRoom from "../implementations/LeaveRoom";
+import MessageRequest from "../implementations/MessageRequest";
+import MessageSent from "../implementations/MessageSent";
 import UserDisconnected from "../implementations/UserDisconnected";
 
 const devEvents:EventConstructor[] = [
-    UserDisconnected
+    UserDisconnected,
+    JoinRoom,
+    MessageRequest,
+    LeaveRoom,
+    MessageSent
 ];
 
 export default devEvents;

f2759be6b5806109a4b4a04cf9833232c3150d1c
Luisfc68 <Luisfc68@gmail.com>
2021-10-19 22:39:07 -0300

    Agrega serializacion en eventos

--- src/services/socket/events/implementations/UserDisconnected.ts
+++ src/services/socket/events/implementations/UserDisconnected.ts
@@ -1,9 +1,12 @@
 import { Socket } from "socket.io";
 import { Injectable } from "../../../../../node_modules/@tsed/di/lib";
+import { serialize } from "@tsed/json-mapper";
 import { $log } from "../../../../../node_modules/@tsed/logger/lib";
 import ChatSocketService from "../../ChatSocketService";
 import { SocketEvents } from "../../SocketEvents";
 import ChatEvent from "../ChatEvent";
+import User from "../../../../business-logic/entity/User";
+import { AppGroups } from "../../../../business-logic/GroupsEnum";
 
 @Injectable()
 export default class UserDisconnected extends ChatEvent{
@@ -17,10 +20,16 @@ export default class UserDisconnected extends ChatEvent{
     }
     
     executeEvent(socket:Socket,service:ChatSocketService,..._args:any):void{
-        
+        console.log(SocketEvents.DISCONECT);
         let user = service.activeUsers.get(socket);
         if(!user) return;
         $log.info(user.username+" se desconecto");
+
+        if(user.actualChat && user.actualChat.id){
+            let serialized = serialize(user,{type: User,groups: [AppGroups.USER]})
+            socket.to(user.actualChat.id).emit(SocketEvents.LEAVE_ROOM,serialized);
+        }
+        
         service.activeUsers.delete(socket);
 
     };

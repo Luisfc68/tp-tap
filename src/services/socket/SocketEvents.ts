a015ebd67ba586d5ab0bb59c731c41d68448b4d9
Luisfc68 <Luisfc68@gmail.com>
2021-11-02 18:00:52 -0300

    Cambia nombres de evento a camel case

--- src/services/socket/SocketEvents.ts
+++ src/services/socket/SocketEvents.ts
@@ -1,18 +1,18 @@
 export enum SocketEvents{
     ERROR="error",
     DISCONECT="disconnect",
-    JOIN_ROOM="join-room",
+    JOIN_ROOM="joinRoom",
     CHAT_REQUEST="chatRequest",
-    MSG_REQUEST="msg-request",
-    LEAVE_ROOM="leave-room",
-    MSG_SENT="msg-sent",
-    CLEAN_LEAVE="clean-leave-room",
+    MSG_REQUEST="msgRequest",
+    LEAVE_ROOM="leaveRoom",
+    MSG_SENT="msgSent",
+    CLEAN_LEAVE="cleanLeaveRoom",
     CONNECTION_SUCCESS="connectionSuccess",
-    ADD_FAV_CHAT="add-fav-chat",
-    RM_FAV_CHAT="rm-fav-chat",
+    ADD_FAV_CHAT="addFavChat",
+    RM_FAV_CHAT="rmFavChat",
     //Estos son eventos que no se generan en tiempo real
     //el servidor los genera, no los responde, por eso no tienen handler
-    USER_CHANGED="user-changed",
-    CHAT_CHANGED="chat-changed"
+    USER_CHANGED="userChanged",
+    CHAT_CHANGED="chatChanged"
 
 }
